f80c69167d1b2d74171b427c53481040569d9b01
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2009-11-27 18:41:59 +0100

    (MT #2264) Migrate allowonshelfholds to smart-rules
    This patch change the way to manage allowonshelfholds, now it's managed in smart-rules and allow to be more granular.
    It need another patch, for search results.
    
    Conflict solved : admin/smart-rules.pl (adding allowonshelfholds and maxpickupdelay all together)

---
+++ installer/data/mysql/atomicupdate/0012-allow-on-shelf-holds.pl
@@ -0,0 +1,16 @@
+#! /usr/bin/perl
+use strict;
+use warnings;
+use C4::Context;
+my $dbh=C4::Context->dbh;
+$dbh->do("ALTER TABLE issuingrules ADD COLUMN `allowonshelfholds` TINYINT(1) NOT NULL DEFAULT '0';");
+
+my $sth = $dbh->prepare( "SELECT value from systempreferences where variable = 'AllowOnShelfHolds';" ); 
+$sth->execute();
+my $data = $sth->fetchrow_hashref();
+
+my $updsth = $dbh->prepare("UPDATE issuingrules SET allowonshelfholds = ?");
+$updsth->execute($data->{value});
+
+$dbh->do("DELETE FROM systempreferences WHERE from systempreferences where variable = 'AllowOnShelfHolds';");
+print "Upgrade done (Migrating AllowOnShelfHold to smart-rules)\n";
