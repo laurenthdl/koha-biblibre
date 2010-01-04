df8a790b161cb2b74a22b88b57605c6fbbd67407
Jean-André Santoni <jeanandre.santoni@biblibre.com>
2010-01-04 17:06:17 +0100

    Fixed a major bug in ModReceiveOrder
    Orders were not splited correctly.

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -1164,7 +1164,7 @@ sub ModReceiveOrder {
                 , unitprice=?
                 , freight=?
                 , rrp=?
-                , quantityreceived=?
+                , quantity=?
             WHERE biblionumber=? AND ordernumber=?");
 
         $sth->execute($quantrec,$datereceived,$invoiceno,$cost,$freight,$rrp,$quantrec,$biblionumber,$ordernumber);
@@ -1174,6 +1174,8 @@ sub ModReceiveOrder {
         foreach my $orderkey ( "linenumber", "allocation" ) {
             delete($order->{'$orderkey'});
         }
+        $order->{'quantity'} -= $quantrec;
+        $order->{'quantityreceived'} = 0;
         my $newOrder = NewOrder($order);
 } else {
         $sth=$dbh->prepare("update aqorders
