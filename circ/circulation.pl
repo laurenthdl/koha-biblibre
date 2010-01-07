1c9a0298cfd7c2025d953033e74838301261020e
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-01-07 23:04:54 +0100

    Bug Fix : Incorrect date tests for overdue
    Dates tests were done on date formatted for output
    So cmp with a metric date or a us date would be incorrect

--- circ/circulation.pl
+++ circ/circulation.pl
@@ -176,7 +176,7 @@ if($duedatespec_allow){
     $datedue = $globalduedate if ($globalduedate);
 }
 
-my $todaysdate = C4::Dates->new->output(C4::Context->preference('dateformat'));
+my $todaysdate = C4::Dates->new;
 
 # check and see if we should print
 if ( $barcode eq '' && $print eq 'maybe' ) {
@@ -459,7 +459,7 @@ if ($borrower) {
 		$it->{'can_renew'} = $can_renew;
 		$it->{'can_confirm'} = !$can_renew && !$restype;
 		$it->{'renew_error'} = $restype;
-	    $it->{'checkoutdate'} = C4::Dates->new($it->{'issuedate'},'iso')->output('syspref');
+	    $it->{'checkoutdate'} = C4::Dates->new($it->{'issuedate'},'iso');
 
 	    $totalprice += $it->{'replacementprice'};
 		$it->{'itemtype'} = $itemtypeinfo->{'description'};
@@ -472,7 +472,7 @@ if ($borrower) {
 	$it->{'branchdisplay'} = GetBranchName((C4::Context->preference('HomeOrHoldingBranch') eq 'holdingbranch') ? $it->{'holdingbranch'} : $it->{'homebranch'});
         # ADDED BY JF: NEW ITEMTYPE COUNT DISPLAY
         $issued_itemtypes_count->{ $it->{'itemtype'} }++;
-        if ( $todaysdate eq $it->{'issuedate'} or $todaysdate eq $it->{'lastreneweddate'} ) {
+        if ( $todaysdate eq $it->{'checkoutdate'} or $todaysdate eq $it->{'lastreneweddate'} ) {
             push @todaysissues, $it;
         } else {
             push @previousissues, $it;
