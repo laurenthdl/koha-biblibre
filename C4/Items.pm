69a320dcb7dfbeeb69c4ea74e8850f4077220c35
Frédéric Demians <f.demians@tamil.fr>
2010-01-15 20:49:34 +0100

    Bug 4047 - Add a link to branch URL if available on holdings in OPAC

--- C4/Items.pm
+++ C4/Items.pm
@@ -1266,7 +1266,8 @@ sub GetItemsInfo {
            biblioitems.lccn,
            biblioitems.url,
            items.notforloan as itemnotforloan,
-           itemtypes.description
+           itemtypes.description,
+           branchurl
      FROM items
      LEFT JOIN branches ON items.homebranch = branches.branchcode
      LEFT JOIN biblio      ON      biblio.biblionumber     = items.biblionumber
