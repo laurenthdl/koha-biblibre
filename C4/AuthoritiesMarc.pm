ab72e71c72b23efe8a20f31b81d0eec0da8893eb
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-01-15 11:04:14 +0100

    SetUTF8Flag on any incoming  data
    This prompts NFC normalization on adding record
    Therefore, any saved record will be NFC normalized

--- C4/AuthoritiesMarc.pm
+++ C4/AuthoritiesMarc.pm
@@ -630,6 +630,7 @@ sub AddAuthority {
         $format= 'MARC21';
     }
 
+    SetUTF8Flag($record);
 	if ($format eq "MARC21") {
 		if (!$record->leader) {
 			$record->leader($leader);
@@ -662,7 +663,7 @@ sub AddAuthority {
 	}
 
   if (($format eq "UNIMARCAUTH") && (!$record->subfield('100','a'))){
-        $record->leader("     nx  j22             ");
+        $record->leader("     nx  j22             ") unless ($record->leader());
         my $date=POSIX::strftime("%Y%m%d",localtime);    
         if ($record->field('100')){
           $record->field('100')->update('a'=>$date."afrey50      ba0");
