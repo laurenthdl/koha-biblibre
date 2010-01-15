fab52aefb32d2fe72616d45cf032ead5fb213145
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-01-15 12:40:48 +0100

    (MT823) add the possibility to have enrolment end date
    this patch add the possibility to have an end date of enrolment, instead of a period. Like for a user category, all this category can have the same expiry date.
    The librarian can configure one of both, but not both.

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3865,6 +3865,14 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
 }
 
 
+
+$DBversion = '3.02.00.011';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do('ALTER TABLE `categories` ADD COLUMN `enrolmentperioddate` DATE NULL DEFAULT NULL AFTER `enrolmentperiod`');
+    print "Upgrade done (Add enrolment period date support)\n";
+    SetVersion ($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
--- kohaversion.pl
+++ kohaversion.pl
@@ -11,7 +11,7 @@ use strict;
 
 sub kohaversion {
 
-    our $VERSION = '3.02.00.010';
+    our $VERSION = '3.02.00.011';
     # version needs to be set this way
     # so that it can be picked up by Makefile.PL
     # during install
