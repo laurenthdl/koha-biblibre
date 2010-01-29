35e50282e50383d264520b9d0f5d7140a5cd2b14
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-01-29 20:08:35 +0100

    Adding EXPIRED flag to Members management
    This flag is raised whenever dateexpiry is before today
    EXPIRED Flag was only set if patron was debarred
    
    followup: 6bcd5fe2413155682ca403f29fe62ffc1610f47d
    EXPIRED flag is added to patronflags
    
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- C4/Members.pm
+++ C4/Members.pm
@@ -392,9 +392,9 @@ The following will be set where applicable:
  $flags->{ LOST  }->{noissues}      Set for each LOST
  $flags->{ LOST  }->{message}       Message -- deprecated
 
- $flags->{DBARRED}                  Set if patron debarred, no access
- $flags->{DBARRED}->{noissues}      Set for each DBARRED
- $flags->{DBARRED}->{message}       Message -- deprecated
+ $flags->{DEBARRED}                  Set if patron debarred, no access
+ $flags->{DEBARRED}->{noissues}      Set for each DEBARRED
+ $flags->{DEBARRED}->{message}       Message -- deprecated
 
  $flags->{ NOTES }
  $flags->{ NOTES }->{message}       The note itself.  NOT deprecated
@@ -408,6 +408,7 @@ The following will be set where applicable:
  $flags->{WAITING}->{message}       Message -- deprecated
  $flags->{WAITING}->{itemlist}      ref-to-array: list of available items
 
+ $flags->{EXPIRED}                  patron subscription expired
 =over 
 
 =item C<$flags-E<gt>{ODUES}-E<gt>{itemlist}> is a reference-to-array listing the
@@ -468,13 +469,22 @@ sub patronflags {
         $flaginfo{'noissues'} = 1;
         $flags{'LOST'}        = \%flaginfo;
     }
-    if (   $patroninformation->{'debarred'}
-        && $patroninformation->{'debarred'} == 1 )
-    {
-        my %flaginfo;
-        $flaginfo{'message'}  = 'Borrower is Debarred.';
-        $flaginfo{'noissues'} = 1;
-        $flags{'DBARRED'}     = \%flaginfo;
+    if ( check_date(split(/-/,$patroninformation->{'dateexpiry'})) ){
+        if(Date_to_Days(Date::Calc::Today) > Date_to_Days(split(/-/,$patroninformation->{'dateexpiry'}) )){
+            my %flaginfo;
+            $flaginfo{'noissues'} = 1;
+            $flags{'EXPIRED'} = \%flaginfo;
+        }
+    }
+    if ( $patroninformation->{'debarred'} && check_date(split(/-/,$patroninformation->{'debarred'})) ){
+        if(Date_to_Days(Date::Calc::Today) < Date_to_Days(split(/-/,$patroninformation->{'debarred'}) )){
+            my %flaginfo;
+            $flaginfo{'message'}  = 'Borrower is Debarred.';
+            $flaginfo{'noissues'} = 1;
+            $flaginfo{'dateend'} = $patroninformation->{'debarred'};
+            $flaginfo{'comment'} = $patroninformation->{'debarredcomment'};
+            $flags{'DEBARRED'}     = \%flaginfo;
+        }
     }
     if (   $patroninformation->{'borrowernotes'}
         && $patroninformation->{'borrowernotes'} )
