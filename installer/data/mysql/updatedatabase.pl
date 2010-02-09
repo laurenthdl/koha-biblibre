78d36714d3943ed97a62ada8168fd9bae5cf66db
Matthias Meusburger <matthias.meusburger@biblibre.com>
2010-02-09 17:13:30 +0100

    MT2950: issuingrules.renewalperiod can be null

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -3487,6 +3487,13 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = '3.02.00.018';
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do("ALTER TABLE `issuingrules` CHANGE `renewalperiod` `renewalperiod` SMALLINT(6) NULL");
+    print "Upgrade done (Updating renewalperiod)\n";
+    SetVersion ($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
