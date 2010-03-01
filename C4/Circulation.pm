1f1a1276f75c4cf0a36a51253af0fed76e25bc4f
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-03-01 13:37:48 +0100

    (MT #3067) change message from issuingimpossible to needsconfirmation
    If a user is blocked, don't block the user for issues, just ask librarian what he want.

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -757,10 +757,10 @@ sub CanBookBeIssued {
     my ( $blocktype, $count ) = C4::Members::IsMemberBlocked( $borrower->{'borrowernumber'} );
     if ( $blocktype == -1 ) {
         ## remaining overdue documents
-        $issuingimpossible{USERBLOCKEDREMAINING} = $count;
+        $needsconfirmation{USERBLOCKEDREMAINING} = $count;
     } elsif ( $blocktype == 1 ) {
         ## blocked because of overdue return
-        $issuingimpossible{USERBLOCKEDOVERDUE} = $count;
+        $needsconfirmation{USERBLOCKEDOVERDUE} = $count;
     }
 
     #
