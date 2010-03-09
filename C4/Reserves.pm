a630f28911f2c521b50d9cd42213dbfa8e6a9c3b
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2010-03-09 22:32:29 +0100

    C4/Reserves.pm : HomeOrHoldingBranch Management
    Conflicts:
    
    	C4/Reserves.pm
    
    Signed-off-by: Henri-Damien LAURENT <henridamien.laurent@biblibre.com>

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -432,8 +432,9 @@ sub CanItemBeReserved{
     my $branchfield  = "reserves.branchcode";
     
     if( $controlbranch eq "ItemHomeLibrary" ){
-        $branchfield = "items.homebranch";
-        $branchcode = $item->{homebranch};
+        my $field=C4::Context->preference("HomeOrHoldingBranch")||"homebranch";
+        $branchfield = "items.$field";
+        $branchcode = $item->{$field};
     }elsif( $controlbranch eq "PatronLibrary" ){
         $branchfield = "borrowers.branchcode";
         $branchcode = $borrower->{branchcode};
@@ -649,6 +650,27 @@ sub GetReserveFee {
         }
     }
     return $fee;
+}
+
+#-------------------------------------------------------------------------------------
+=item GetReservesControlBranch
+
+$branchcode = &GetReservesControlBranch($borrower, $item)
+
+Returns the branchcode to consider to check hold rules against
+
+=cut
+sub GetReservesControlBranch{
+    my ($borrower, $item) = @_;
+    my $controlbranch = C4::Context->preference('ReservesControlBranch');
+    my $hbr           = C4::Context->preference('HomeOrHoldingBranch')||"homebranch";
+    my $branchcode="*";
+    if($controlbranch eq "ItemHomeLibrary"){
+        $branchcode = $item->{$hbr};
+    } elsif($controlbranch eq "PatronLibrary"){
+        $branchcode = $borrower->{'branchcode'};
+    }
+    return $branchcode;
 }
 
 =item GetReservesToBranch
