e6f531fe998bc931e704c3ac2220d37f9423d7a4
Nahuel ANGELINETTI <nahuel.angelinetti@biblibre.com>
2010-03-16 17:11:10 +0100

    (bug #4319) allow reserves on waiting items
    This add a function to check the reserve status, and add a check in IsAvailableForItemLevelRequest() to allow request on items that are waiting

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -98,7 +98,8 @@ BEGIN {
         &GetReserveCount
         &GetReserveFee
 		&GetReserveInfo
-    
+        &GetReserveStatus
+        
         &GetOtherReserves
         
         &ModReserveFill
@@ -582,6 +583,18 @@ sub GetReservesForBranch {
         $i++;
     }
     return (@transreserv);
+}
+
+sub GetReserveStatus {
+    my ($itemnumber) = @_;
+    
+    my $dbh = C4::Context->dbh;
+    
+    my $itemstatus = $dbh->prepare("SELECT found FROM reserves WHERE itemnumber = ?");
+    
+    $itemstatus->execute($itemnumber);
+    my ($found) = $itemstatus->fetchrow_array;
+    return $found;
 }
 
 =item CheckReserves
@@ -1177,10 +1190,11 @@ sub IsAvailableForItemLevelRequest {
                                $item->{wthdrawn} or
                                $notforloan_per_itemtype;
 
+    
     if (C4::Context->preference('AllowOnShelfHolds')) {
         return $available_per_item;
     } else {
-        return ($available_per_item and $item->{onloan}); 
+        return ($available_per_item and ($item->{onloan} or GetReserveStatus($itemnumber) eq "W")); 
     }
 }
 
