liblime/LibLime-Koha

Bug 4239: Allow Multiple Item Level Holds For A Single Record
This allows multiple items per record to be placed on hold be a single patron.
To enable, enter a list of space separated itemtype codes in the syspref AllowMultipleHoldsPerBib ( i.e. "MAG JMAG YMAG" ).
Useful for magazines, encyclopedias and other bibs where the attached items are not identical.

Mandatory SQL:
ALTER TABLE `reserves` ADD COLUMN `reservenumber` int(11) NOT NULL;
ALTER TABLE `old reserves` ADD COLUMN `reservenumber` int(11) NOT NULL;
INSERT INTO `systempreferences` ( `variable` , `value` , `options` , `explanation` , `type` ) VALUES ( 'AllowMultipleHoldsPerBib', '', '', 'This allows multiple items per record to be placed on hold be a single patron. To enable, enter a list of space separated itemtype codes in the field ( i.e. "MAG JMAG YMAG" ). Useful for magazines, encyclopedias and other bibs where the attached items are not identical.', '' );

reserve/modrequest.pl
@@ -42,6 +42,7 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
 );
 
 my @rank=$query->param('rank-request');
+my @reservenumber=$query->param('reservenumber');
 my @biblionumber=$query->param('biblionumber');
 my @borrower=$query->param('borrowernumber');
 my @branch=$query->param('pickup');
@@ -54,9 +55,9 @@ my $CancelBiblioNumber=$query->param('CancelBiblioNumber');
 my $CancelBorrowerNumber=$query->param('CancelBorrowerNumber');
 my $CancelItemnumber=$query->param('CancelItemnumber');
 
-# 2 possibilitys : cancel an item reservation, or modify or cancel the queded list
+# 2 possibilitys : cancel an item reservation, or modify or cancel the queued list
 
-# 1) cancel an item reservation by fonction ModReserveCancelAll (in reserves.pm)
+# 1) cancel an item reservation by function ModReserveCancelAll (in reserves.pm)
 if ($CancelBorrowerNumber) {
     ModReserveCancelAll($CancelItemnumber, $CancelBorrowerNumber);
     $biblionumber[0] = $CancelBiblioNumber,
@@ -66,7 +67,7 @@ if ($CancelBorrowerNumber) {
 else {
     for (my $i=0;$i<$count;$i++){
         undef $itemnumber[$i] unless $itemnumber[$i] ne '';
-        ModReserve($rank[$i],$biblionumber[$i],$borrower[$i],$branch[$i],$itemnumber[$i]); #from C4::Reserves
+        ModReserve($rank[$i],$biblionumber[$i],$borrower[$i],$branch[$i],$itemnumber[$i],$reservenumber[$i]); #from C4::Reserves
     }
 }
 my $from=$query->param('from');

laurenthdl/koha-biblibre

reserve/modrequest.pl
@@ -41,12 +41,13 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     }
 );
 
-my @rank=$query->param('rank-request');
-my @biblionumber=$query->param('biblionumber');
-my @borrower=$query->param('borrowernumber');
-my @branch=$query->param('pickup');
-my @itemnumber=$query->param('itemnumber');
-my $multi_hold = $query->param('multi_hold');
+my @rank          = $query->param('rank-request');
+my @reservenumber = $query->param('reservenumber');
+my @biblionumber  = $query->param('biblionumber');
+my @borrower      = $query->param('borrowernumber');
+my @branch        = $query->param('pickup');
+my @itemnumber    = $query->param('itemnumber');
+my $multi_hold    = $query->param('multi_hold');
 my $biblionumbers = $query->param('biblionumbers');
 my $count=@rank;
 
@@ -54,9 +55,9 @@ my $CancelBiblioNumber=$query->param('CancelBiblioNumber');
 my $CancelBorrowerNumber=$query->param('CancelBorrowerNumber');
 my $CancelItemnumber=$query->param('CancelItemnumber');
 
-# 2 possibilitys : cancel an item reservation, or modify or cancel the queded list
+# 2 possibilitys : cancel an item reservation, or modify or cancel the queued list
 
-# 1) cancel an item reservation by fonction ModReserveCancelAll (in reserves.pm)
+# 1) cancel an item reservation by function ModReserveCancelAll (in reserves.pm)
 if ($CancelBorrowerNumber) {
     ModReserveCancelAll($CancelItemnumber, $CancelBorrowerNumber);
     $biblionumber[0] = $CancelBiblioNumber,
@@ -66,7 +67,7 @@ if ($CancelBorrowerNumber) {
 else {
     for (my $i=0;$i<$count;$i++){
         undef $itemnumber[$i] unless $itemnumber[$i] ne '';
-        ModReserve($rank[$i],$biblionumber[$i],$borrower[$i],$branch[$i],$itemnumber[$i]); #from C4::Reserves
+        ModReserve($rank[$i],$biblionumber[$i],$borrower[$i],$branch[$i],$itemnumber[$i],$reservenumber[$i]); #from C4::Reserves
     }
 }
 my $from=$query->param('from');