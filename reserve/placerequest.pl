49605054aee05fb0e66997db05193cb84ca7eea7
Paul POULAIN <paul.poulain@biblibre.com>
2010-10-30 04:31:15 +0200

    merge errors (missing functions,...)

--- reserve/placerequest.pl
+++ reserve/placerequest.pl
@@ -3,7 +3,6 @@
 #script to place reserves/requests
 #writen 2/1/00 by chris@katipo.oc.nz
 
-
 # Copyright 2000-2002 Katipo Communications
 #
 # This file is part of Koha.
@@ -37,34 +36,34 @@ my $input = CGI->new();
 
 my ($user, $cookie, $sesion_id, $flags) = checkauth($input, 0, { reserveforothers => 'place_holds' }, 'intranet');
 
-my @bibitems=$input->param('biblioitem');
+my @bibitems = $input->param('biblioitem');
 # FIXME I think reqbib does not exist anymore, it's used in line 82, to AddReserve of contraint type 'o'
 #       I bet it's a 2.x feature, reserving a given biblioitem, that is useless in Koha 3.0
 #       we can remove this line, the AddReserve of constrainttype 'o',
 #       and probably remove the reserveconstraint table as well, I never could fill anything in this table.
-#my @reqbib=$input->param('reqbib'); 
-my $biblionumber=$input->param('biblionumber');
-my $borrower=$input->param('member');
-my $notes=$input->param('notes');
-my $branch=$input->param('pickup');
-my $startdate=$input->param('reserve_date') || '';
-my @rank=$input->param('rank-request');
-my $type=$input->param('type');
-my $title=$input->param('title');
-my $borrowernumber=GetMember('cardnumber'=>$borrower);
+#my @reqbib         = $input->param('reqbib');
+my $biblionumber   = $input->param('biblionumber');
+my $borrower       = $input->param('member');
+my $notes          = $input->param('notes');
+my $branch         = $input->param('pickup');
+my $startdate      = $input->param('reserve_date') || '';
+my @rank           = $input->param('rank-request');
+my $type           = $input->param('type');
+my $title          = $input->param('title');
+my $borrowernumber = GetMember( 'cardnumber' => $borrower );
 my @checkitems     = $input->param('checkitem');
 my $expirationdate = $input->param('expiration_date');
 
-my $multi_hold = $input->param('multi_hold');
-my $biblionumbers = $multi_hold ? $input->param('biblionumbers') : ($biblionumber . '/');
-my $bad_bibs = $input->param('bad_bibs');
+my $multi_hold    = $input->param('multi_hold');
+my $biblionumbers = $multi_hold ? $input->param('biblionumbers') : ( $biblionumber . '/' );
+my $bad_bibs      = $input->param('bad_bibs');
 
 my %bibinfos = ();
 my @biblionumbers = split '/', $biblionumbers;
 foreach my $bibnum (@biblionumbers) {
     my %bibinfo = ();
-    $bibinfo{title} = $input->param("title_$bibnum");
-    $bibinfo{rank} = $input->param("rank_$bibnum");
+    $bibinfo{title}    = $input->param("title_$bibnum");
+    $bibinfo{rank}     = $input->param("rank_$bibnum");
     $bibinfos{$bibnum} = \%bibinfo;
 }
 
@@ -74,27 +73,26 @@ my $found;
 # of the document, we force the value $rank and $found .
 for my $item (@checkitems){
     if ( $item ne '' ) {
-    $rank[0] = '0' unless C4::Context->preference('ReservesNeedReturns');
-    my $item = $checkitem;
-    $item = GetItem($item);
-    if ( $item->{'holdingbranch'} eq $branch ){
-        $found = 'W' unless C4::Context->preference('ReservesNeedReturns');
+        $rank[0] = '0' unless C4::Context->preference('ReservesNeedReturns');
+        my $itemdata = GetItem($item);
+        if ( $itemdata->{'holdingbranch'} eq $branch ) {
+            $found = 'W' unless C4::Context->preference('ReservesNeedReturns');
+        }
     }
 }
-}
 
-if ($type eq 'str8' && $borrowernumber ne ''){
+if ( $type eq 'str8' && $borrowernumber ne '' ) {
 
-    foreach my $biblionumber (keys %bibinfos) {
-        my $count=@bibitems;
-        @bibitems=sort @bibitems;
-        my $i2=1;
+    foreach my $biblionumber ( keys %bibinfos ) {
+        my $count = @bibitems;
+        @bibitems = sort @bibitems;
+        my $i2 = 1;
         my @realbi;
-        $realbi[0]=$bibitems[0];
-        for (my $i=1;$i<$count;$i++) {
-            my $i3=$i2-1;
-            if ($realbi[$i3] ne $bibitems[$i]) {
-                $realbi[$i2]=$bibitems[$i];
+        $realbi[0] = $bibitems[0];
+        for ( my $i = 1 ; $i < $count ; $i++ ) {
+            my $i3 = $i2 - 1;
+            if ( $realbi[$i3] ne $bibitems[$i] ) {
+                $realbi[$i2] = $bibitems[$i];
                 $i2++;
             }
         }
@@ -126,8 +124,8 @@ if ($type eq 'str8' && $borrowernumber ne ''){
     } else {
         print $input->redirect("request.pl?biblionumber=$biblionumber");
     }
-} elsif ($borrowernumber eq ''){
-	print $input->header();
-	print "Invalid card number please try again";
-	print $input->Dump;
+} elsif ( $borrowernumber eq '' ) {
+    print $input->header();
+    print "Invalid card number please try again";
+    print $input->Dump;
 }
