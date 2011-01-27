0407f94c5585a5260adea0199bf40b418f0f6e15
Jonathan Druart <jonathan.druart@biblibre.com>
2011-01-27 10:36:10 +0100

    MT5698: Add statistics in member module And columns in circulation history
    3 statistics : Issue count, Total issues in last year, Total issues
    3 collumns : item type, ccode and location(section)

--- members/readingrec.pl
+++ members/readingrec.pl
@@ -88,6 +88,9 @@ foreach my $issue ( @{$issues} ) {
     $line{renewals}        = $issue->{'renewals'};
     $line{barcode}         = $issue->{'barcode'};
     $line{volumeddesc}     = $issue->{'volumeddesc'};
+    $line{itemtype}        = $issue->{'itemtype'};
+    $line{location}        = $issue->{'location'};
+    $line{ccode}           = $issue->{'ccode'};
     push( @loop_reading, \%line );
 }
 
