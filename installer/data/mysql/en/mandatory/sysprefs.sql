e004b027a83d212b90cad1be5168e56981c454d3
Stéphane Delaune <stephane.delaune@biblibre.com>
2011-03-10 14:41:33 +0100

    (MT #4736_electre) add functions to get biblio informations from the electre's web service

--- installer/data/mysql/en/mandatory/sysprefs.sql
+++ installer/data/mysql/en/mandatory/sysprefs.sql
@@ -300,3 +300,11 @@ INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES (
 INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('BlockRenewWhenOverdue','0','Blocks renewals when overdue','','YesNo');
 INSERT INTO `systempreferences` (variable,value,options,explanation,type) VALUES('SolrAPI','http://localhost:8080/solr','','Solr web service URL.','Free');
 INSERT INTO `systempreferences` (variable,value,options,explanation,type) VALUES('SearchEngine','Solr','Solr|Zebra','Search Engine','Choice');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('ElectreLogin', '', 'Login for Electre ws',NULL,'free');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('ElectrePassw', '', 'Password for Electre ws',NULL,'free');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreImage', '0', 'if ON, enable cover displaying from Electre ws',NULL,'YesNo');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreScaledImage', '1', 'if ON, enable scaling of cover from Electre ws',NULL,'YesNo');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreQuatriemeXml', '0', 'if ON, enable outside back cover displaying from Electre ws',NULL,'YesNo');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreResume', '0', 'if ON, enable resume displaying from Electre ws',NULL,'YesNo');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreSearchResulstImage', '0', 'if ON, enable cover displaying from Electre ws on OPAC search results',NULL,'YesNo');
+INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) VALUES('OpacElectreDisplayOnTab', '0', 'if ON, Electre informations (outside back cover and resume) are displayed in a dedicated Tab instead of being in the header',NULL,'YesNo');
