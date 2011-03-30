8cb378e2f61f7a73b44187a94298acca5e7adcb2
Claire Hernandez <claire.hernandez@biblibre.com>
2011-03-30 16:58:12 +0200

    [SOLR] MT6310 Fix pagination issue - bad results number when 2 params are set in advanced search

--- catalogue/search.pl
+++ catalogue/search.pl
@@ -484,7 +484,8 @@ my @pager_params = map { {
     ind => 'filters',
     val => $_->{'ind'}.':"'.$_->{'val'}.'"'
 } } @tplfilters;
-push @pager_params, { ind => 'q'      , val => $cgi->param('q') };
+push @pager_params, map { { ind => 'q'      , val => $_ } } $cgi->param('q');
+push @pager_params, map { { ind => 'idx'      , val => $_ } } $cgi->param('idx');
 push @pager_params, { ind => 'sort_by', val => $sort_by         };
 
 # Pager template params
