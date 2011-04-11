ebe405024503ba39d19846b3c0a45e9adb90d601
Stéphane Delaune <stephane.delaune@biblibre.com>
2011-04-11 16:14:57 +0200

    (MT #6413) add syspref SubfieldsToDiscardWhenPrefill
    Conflicts solved:
    
    	installer/data/mysql/en/mandatory/sysprefs.sql
    	installer/data/mysql/updatedatabase.pl

--- installer/data/mysql/en/mandatory/sysprefs.sql
+++ installer/data/mysql/en/mandatory/sysprefs.sql
@@ -306,3 +306,6 @@ INSERT IGNORE INTO systempreferences (variable,value,explanation,options,type) V
 INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('PrefillItem','0','When a new item is added, should it be prefilled with last created item values?','','YesNo');
 INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('uploadPath','','Sets the upload path for the upload.pl plugin','','');
 INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('uploadWebPath','','Set the upload path starting from document root for the upload.pl plugin','','');
+INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('BlockRenewWhenOverdue','0','Blocks renewals when overdue','','YesNo');
+INSERT INTO `systempreferences` (variable,value,explanation,options,type) VALUES('numSearchRSSResults',50,'Specify the maximum number of results to display on a RSS page of results',NULL,'Integer');
+INSERT INTO systempreferences (variable,value,explanation,options,type) VALUES ('SubfieldsToDiscardWhenPrefill','f u','define a list of subfields to discard when prefill (separated by space)','','Free');
