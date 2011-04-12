e9e3b131134ca3c0bee96ed592da6b7083c2fc1d
Claire Hernandez <claire.hernandez@biblibre.com>
2011-04-12 10:46:12 +0200

    MT4735: Adds the ability to convert records from MARC21 to UNIMARC
    Conflicts solved:
    
    	installer/data/mysql/updatedatabase.pl
    	kohaversion.pl

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5350,6 +5350,16 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.06.00.067";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    $dbh->do(qq{
+    ALTER TABLE `z3950servers` ADD `xslt` VARCHAR( 255 ) NULL AFTER `encoding`;
+    });
+    print "Upgrade to $DBversion done (Adds xslt in z3950servers table)\n";
+    SetVersion($DBversion);
+}
+
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
