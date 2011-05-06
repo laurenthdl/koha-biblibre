fd5b97de3017564347a38e145b615dde61e0edd9
Matthias Meusburger <matthias.meusburger@biblibre.com>
2011-05-06 11:27:22 +0200

    MT6523: Receving improvements
        - Items and fund are displayed

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -1620,6 +1620,7 @@ sub GetParcel {
                 aqorders.ecost,
                 aqorders.parent_ordernumber,
                 aqorders.gstrate,
+		aqorders.budget_id,
                 biblio.title
         FROM aqorders
         LEFT JOIN aqbasket ON aqbasket.basketno=aqorders.basketno
