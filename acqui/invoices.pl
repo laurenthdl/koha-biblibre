88aac2bb0b634d509c4c0f5a9e46c424d97e5fb1
Julian Maurice <julian.maurice@biblibre.com>
2011-05-10 17:56:39 +0200

    MT6533: New page invoices.pl (wip)

---
+++ acqui/invoices.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl
+
+# Copyright 2011 BibLibre SARL
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Koha; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+=head1 NAME
+
+invoices.pl
+
+=head1 DESCRIPTION
+
+Search for invoices
+
+=cut
+
+use strict;
+use warnings;
+
+use CGI;
+use C4::Auth;
+use C4::Output;
+
+use C4::Bookseller;
+use C4::Branch;
+
+my $input = new CGI;
+my ($template, $loggedinuser, $cookie, $flags) = get_template_and_user( {
+    template_name   => 'acqui/invoices.tmpl',
+    query           => $input,
+    type            => 'intranet',
+    authnotrequired => 0,
+    flagsrequired   => { 'acquisition' => '*' },
+    debug           => 1,
+} );
+
+my $invoicenumber   = $input->param('invoicenumber');
+my $supplier        = $input->param('supplier');
+my $billingdatefrom = $input->param('billingdatefrom');
+my $billingdateto   = $input->param('billingdateto');
+my $isbnean         = $input->param('isbnean');
+my $title           = $input->param('title');
+my $author          = $input->param('author');
+my $publisher       = $input->param('publisher');
+my $publicationyear = $input->param('publicationyear');
+my $branch          = $input->param('branch');
+
+
+# Build suppliers list
+my @suppliers = GetBookSeller(undef);
+my @suppliers_loop = ();
+foreach (@suppliers) {
+    my $selected = 1 if $supplier == $_->{'id'};
+    my %row = (
+        suppliername => $_->{'name'},
+        supplierid   => $_->{'id'},
+        selected     => $selected,
+    );
+    push @suppliers_loop, \%row;
+}
+
+# Build branches list
+my $branches = GetBranches();
+my @branches_loop = ();
+foreach (sort keys %$branches) {
+    my $selected = 1 if ($branch eq $_);
+    my %row = (
+        branchcode => $_,
+        branchname => $branches->{$_}->{'branchname'},
+        selected   => $selected,
+    );
+    push @branches_loop, \%row;
+}
+
+$template->param(
+    invoicenumber   => $invoicenumber,
+    supplier        => $supplier,
+    billingdatefrom => $billingdatefrom,
+    billingdateto   => $billingdateto,
+    isbnean         => $isbnean,
+    title           => $title,
+    author          => $author,
+    publisher       => $publisher,
+    publicationyear => $publicationyear,
+    branch          => $branch,
+    suppliers_loop  => \@suppliers_loop,
+    branches_loop   => \@branches_loop,
+);
+
+output_html_with_http_headers $input, $cookie, $template->output;
