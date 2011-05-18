411e55219a45c9cdc3b949dacb525fee9c161581
Matthias Meusburger <matthias.meusburger@biblibre.com>
2011-05-18 13:42:02 +0200

    MT4259, Follow-up: Uses items.new mapping
    Conflicts solved:
    
    	etc/solr
    	kohaversion.pl
    	misc/cronjobs/toggle_new_status.yaml.orig

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5359,6 +5359,14 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.06.00.068";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    $dbh->do(qq{
+    ALTER TABLE `items` ADD `new` INT( 1 ) NULL AFTER `statisticvalue`;
+    });
+    print "Upgrade to $DBversion done (Adds new in items table)\n";
+    SetVersion($DBversion);
+}
 
 =item DropAllForeignKeys($table)
 
