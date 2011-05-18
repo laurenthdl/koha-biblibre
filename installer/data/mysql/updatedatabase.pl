2c5474c1c844404b64f5ea9dbd63c0582f55c552
Matthias Meusburger <matthias.meusburger@biblibre.com>
2011-05-18 13:42:02 +0200

    MT4259, Follow-up: Uses items.new mapping
    Conflicts solved:
    
    	etc/solr
    	kohaversion.pl
    	misc/cronjobs/toggle_new_status.yaml.orig

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5308,6 +5308,15 @@ if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
     SetVersion($DBversion);
 }
 
+$DBversion = "3.06.00.068";
+if ( C4::Context->preference("Version") < TransformToNum($DBversion) ) {
+    $dbh->do(qq{
+    ALTER TABLE `items` ADD `new` INT( 1 ) NULL AFTER `statisticvalue`;
+    });
+    print "Upgrade to $DBversion done (Adds new in items table)\n";
+    SetVersion($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
--- misc/cronjobs/toggle_new_status.pl
+++ misc/cronjobs/toggle_new_status.pl
@@ -5,18 +5,20 @@ use strict;
 use Getopt::Long;
 use YAML;
 use C4::Context;
-use C4::Items qw/ModItem/;
+use C4::Items qw/ModItem ModItemFromMarc/;
 use C4::Biblio;
 
 # Getting options
 my $ccode;
 my $specific;
+my $mapping;
 my $result           = GetOptions(
     'ccode'        => \$ccode,
     'specific'     => \$specific,
+    'mapping'      => \$mapping,
 );
 
-if (not $ccode and not $specific) {
+if (not $ccode and not $specific and not $mapping) {
     print_usage();
     exit 0;
 }
@@ -25,22 +27,30 @@ if (not $ccode and not $specific) {
 my $dbh = C4::Context->dbh;
 
 # Load yaml file
-my ($conf, $rules);
-eval { ($conf,  $rules) = YAML::LoadFile('toggle_new_status.yaml') };
+my ($conf, $rules, $mappingrules);
+eval { ($conf,  $rules, $mappingrules) = YAML::LoadFile('toggle_new_status.yaml') };
 if ($@) {
     print "Unable to load yaml file : $@";
     exit -1;
 }
 
-# Getting new tag
+# Getting specific new tag
 my ($nfield, $nsubfield) = split(/\$/, $conf->{'new_status_tag'});
+print "\nspecific new tag: $nfield\$$nsubfield";
 
-# Getting itype tag
+# Getting items.itype tag
 my ($ifield, $isubfield)  = GetMarcFromKohaField('items.itype', '');
+print "\nitems.itype tag: $ifield\$$isubfield";
+
+# Getting items.new tag
+my ($newfield, $newsubfield)  = GetMarcFromKohaField('items.new', '');
+print "\nitems.new tag: $newfield\$$newsubfield";
 
 
 # Querying based on rules
+if ($ccode or $specific) {
 foreach my $rule (@$rules) {
+    print "\nFirst set of rules: --ccode and/or --specific";
     print "\nrule: ccode " . $rule->{'ccode'} . " / duration : " . $rule->{'duration'} . "\n";
 
     my $query = "select biblionumber, itemnumber from items where ccode=? ";
@@ -79,6 +89,41 @@ foreach my $rule (@$rules) {
 	}
     }
 }
+}
+
+if ($mapping) {
+
+    foreach my $rule (@$mappingrules) {
+	print "\nSecond set of rules: --mapping";
+	print "\nrule: " . $conf->{'mapping_rules_index'} . " " . $rule->{'index'} . " / duration : " . $rule->{'duration'} . "\n";
+
+     my $query = "select biblionumber, itemnumber from items where " .  $conf->{'mapping_rules_index'} . "=? ";
+	$query .= "and TO_DAYS(NOW()) - TO_DAYS(dateaccessioned) >= ? "; 
+	$query .= "and items.new=1";
+	my $sth = $dbh->prepare($query);
+	$sth->execute($rule->{'index'},  $rule->{'duration'});
+
+	while (my $rs = $sth->fetchrow_hashref) {
+	    my $biblionumber = $rs->{biblionumber};
+	    my $itemnumber = $rs->{itemnumber};
+	    my $itemrec = C4::Items::GetMarcItem($biblionumber, $itemnumber);
+	    my $item = $itemrec->field($newfield);
+
+	    if ($item) {
+		# Removes new status 
+		if ($item->subfield($newsubfield)) {
+		   print "Processing item $itemnumber: ";
+		   print "removing $newfield\$$newsubfield from item\n";
+		   $item->delete_subfield($newsubfield);
+		   ModItem({new => undef}, $biblionumber, $itemnumber, C4::Context->dbh, GetFrameworkCode($biblionumber), $item);
+		}
+	    } else {
+		print "Error: can't get item $itemnumber";
+	    }
+	}
+    }
+}
+
 print "done\n";
 
 sub print_usage {
@@ -91,6 +136,8 @@ Parameters (at least one):
     --ccode : replace the "new" itype to a ccode (specified in the config file)
 
     --specific : removes the subfield that contains the "new" status
+
+    --mapping : use the "items.new" mapping 
 
 _USAGE_
 }
