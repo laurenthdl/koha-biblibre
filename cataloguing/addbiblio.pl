0e2a6b5944b4e1f38c62cf5950ce1c4f8cc3232d
BibLibre <dev_patches@biblibre.com>
2011-05-27 15:00:08 +0200

    MT6063 : addbiblio.pl : authorised values with 0
    Do the same as the community/master
    It seems that the previous solution was not that handy and introduced bugs.

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -288,7 +288,7 @@ sub create_input {
     }
 
     # if there is no value provided but a default value in parameters, get it
-    unless ($value) {
+    if ($value eq '' ) {
         $value = $tagslib->{$tag}->{$subfield}->{defaultvalue};
 
         # get today date & replace YYYY, MM, DD if provided in the default value
