f8759c468ebc36877f651193f7205bebfe9e3d01
Jonathan Druart <jonathan.druart@biblibre.com>
2011-06-01 16:22:55 +0200

    MT5553: Add Unique constraint on budget_code field in DB

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5905,6 +5905,13 @@ if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
     SetVersion ($DBversion);
 }
 
+$DBversion = "3.06.00.031";
+if (C4::Context->preference("Version") < TransformToNum($DBversion)) {
+    $dbh->do(q{ALTER TABLE `aqbudgets` ADD UNIQUE ( budget_code );});
+    print "Upgrade to $DBversion done (Unique constraint on budget_code field.)\n";
+    SetVersion ($DBversion);
+}
+
 =item DropAllForeignKeys($table)
 
   Drop all foreign keys of the table $table
