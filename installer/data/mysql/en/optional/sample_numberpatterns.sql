890d9d095e1ce8552cf407e80b3a5229657fe39f
Julian Maurice <julian.maurice@biblibre.com>
2011-06-13 10:33:35 +0200

    MT6714: Change some default values in updatedatabase.pl
    and in sample_numberpatterns.sql

--- installer/data/mysql/en/optional/sample_numberpatterns.sql
+++ installer/data/mysql/en/optional/sample_numberpatterns.sql
@@ -1,7 +1,6 @@
 INSERT INTO `subscription_numberpatterns` (`id`, `label`, `displayorder`, `description`, `numberingmethod`, `label1`, `basedon1`, `add1`, `every1`, `whenmorethan1`, `setto1`, `numbering1`, `label2`, `basedon2`, `add2`, `every2`, `whenmorethan2`, `setto2`, `numbering2`, `label3`, `basedon3`, `add3`, `every3`, `whenmorethan3`, `setto3`, `numbering3`) VALUES
     (1, 'Number', 1, 'Simple Numbering method', 'No.{X}', 'Number', NULL, 1, 1, 99999, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    (2, 'Volume, Number, Issue', 2, 'Volume Number Issue 1', 'Vol.{X}, Number {Y}, Issue {Z}', 'Volume', 'value2', 1, NULL, 99999, 1, NULL, 'Number', 'value3', 1, NULL, 99999, 1, NULL, 'Issue', NULL, 1, 1, 99999, 1, NULL),
-    (3, 'Volume, Number', 3, 'Volume Number 1', 'Vol {X}, No {Y}', 'Volume', 'value2', 1, NULL, 99999, 1, NULL, 'Number', NULL, 1, 1, 99999, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    (4, 'Seasonal', 4, 'Season Year ', '{X} {Y}', 'Season', NULL, 1, 1, 4, 1, 'season', 'Year', 'year', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-    (5, 'Year/Number', 4, 'Year Number 1', 'Year {X}/Number {Y}', 'Year', 'year', NULL, NULL, NULL, NULL, NULL, 'Number', NULL, 1, 1, 99999, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+    (2, 'Volume, Number, Issue', 2, 'Volume Number Issue 1', 'Vol.{X}, Number {Y}, Issue {Z}', 'Volume', NULL, 1, 48, 99999, 1, NULL, 'Number', NULL, 1, 4, 12, 1, NULL, 'Issue', NULL, 1, 1, 4, 1, NULL),
+    (3, 'Volume, Number', 3, 'Volume Number 1', 'Vol {X}, No {Y}', 'Volume', NULL, 1, 12, 99999, 1, NULL, 'Number', NULL, 1, 1, 12, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (4, 'Seasonal', 4, 'Season Year', '{X} {Y}', 'Season', NULL, 1, 1, 4, 1, 'season', 'Year', NULL, 1, 4, 99999, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
