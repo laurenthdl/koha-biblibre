6ef595ace8bcc8acb216dc406f99959c4eac3fb4
Julian Maurice <julian.maurice@biblibre.com>
2011-06-29 15:27:55 +0200

    MT6714: Minor template modifications
    for subscription-detail.pl and subscription-add.pl

--- serials/subscription-add.pl
+++ serials/subscription-add.pl
@@ -115,6 +115,11 @@ if ( $op eq 'mod' || $op eq 'dup' || $op eq 'modsubscription' ) {
             $op         => 1,
             sublength   => $sub_length,
         );
+
+        my ($serials_number) = GetSerials($subscriptionid);
+        if($serials_number > 1) {
+            $template->param(more_than_one_serial => 1);
+        }
     }
     if ( $op eq 'dup' ) {
         my $dont_copy_fields = C4::Context->preference('SubscriptionDuplicateDroppedInput');
