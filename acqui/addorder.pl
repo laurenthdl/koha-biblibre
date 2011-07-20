49e7895e8f9f76c8e8d5053e5c6c088c906f9004
Julian Maurice <julian.maurice@biblibre.com>
2011-07-20 10:25:00 +0200

    MT7249: Fix the "O to disable" feature
    The test is done on ($budget_expend+0) instead of ($budget_expend), to
    enforce number context. The same is done with $bugdet_encumb.
    + Fix template

--- acqui/addorder.pl
+++ acqui/addorder.pl
@@ -148,8 +148,8 @@ unless($confirm_budget_exceeding) {
     my $budget_expenditure = $budget->{'budget_expend'};
 
     if ( $total > $budget_remaining
-      || ($budget_encumbrance && ($budget_used + $total) > $budget_encumbrance)
-      || ($budget_expenditure && ($budget_used + $total) > $budget_expenditure) )
+      || ( ($budget_encumbrance+0) && ($budget_used + $total) > $budget_encumbrance)
+      || ( ($budget_expenditure+0) && ($budget_used + $total) > $budget_expenditure) )
     {
         my ($template, $loggedinuser, $cookie) = get_template_and_user({
             template_name   => "acqui/addorder.tmpl",
@@ -194,7 +194,7 @@ unless($confirm_budget_exceeding) {
         my @mandatory_loop;
         push @mandatory_loop, {value => $_} for $input->param('mandatory');
 
-        if( $budget_encumbrance && ($budget_used + $total) > $budget_encumbrance
+        if( ($budget_encumbrance+0) && ($budget_used + $total) > $budget_encumbrance
           && $total <= $budget_remaining)
         {
             $template->param(
@@ -202,7 +202,7 @@ unless($confirm_budget_exceeding) {
                 encumbrance => sprintf("%.2f", $budget->{'budget_encumb'}),
             );
         }
-        if( $budget_expenditure && ($budget_used + $total) > $budget_expenditure
+        if( ($budget_expenditure+0) && ($budget_used + $total) > $budget_expenditure
           && $total <= $budget_remaining )
         {
             my $currency = GetCurrency;
