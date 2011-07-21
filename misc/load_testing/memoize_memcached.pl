fdce61ed64bcd4d9da57f98d00a41d9990aaa778
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2011-07-21 05:27:44 +0200

    Some Simple Load Testing pages
    those pages may help you profiling the server.
    How so ?
    Edit the files so as to change kohauser and password to what they should be in your context.
    (That was meant in order to be out of any consideration of ... say C4::Context)
    Then link the directory inside koha-tmpl
    
    On a remote computer,
    launch
    for p (memcached.pl memoize_memcached.pl simplepage.pl sysprefs_fields.pl sysprefs_limited.pl tablesysprefs.pl)
    do
        ab -n 500 -c 50 http://opac/cgi-bin/koha/load_testing/$p &>logs_$p
    done
    
    And you're done.
    Be aware though that those are NOT true Koha pages, and they are much lighter.
    But this is a nice start to test

---
+++ misc/load_testing/memoize_memcached.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+ 
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# Koha is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Koha; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+# Suite 330, Boston, MA  02111-1307 USA
+
+use strict;
+use warnings;
+use HTML::Template::Pro;
+use CGI;
+use DBI;
+use YAML;
+use Memoize::Memcached;
+
+my $memcached = {
+            servers    => [ 'unix:/tmp/memcached.sock' ],
+            key_prefix => 'koha',
+        }; 
+memoize_memcached('sysprefs', memcached => $memcached, expire_time => 600000); #cache for 10 minutes
+my $query = new CGI;
+
+    my $template       = HTML::Template::Pro->new(
+        filename          => "./about.tmpl",
+        die_on_bad_params => 1,
+        global_vars       => 1,
+        case_sensitive    => 1,
+        loop_context_vars => 1, # enable: __first__, __last__, __inner__, __odd__, __counter__
+        path              => ["."]
+    );
+
+my $osVersion     = `uname -a`;
+my $perl_path = $^X;
+my $perlVersion   = $];
+my $mysqlVersion  = `mysql -V`;
+my $apacheVersion = (`/usr/sbin/apache2 -V`)[0] ;
+my $zebraVersion = `zebraidx -V`;
+
+my $sysprefs_arrayref = sysprefs();
+$template->param(
+    osVersion     => $osVersion,
+    perlPath      => $perl_path,
+    perlVersion   => $perlVersion,
+    perlIncPath   => [ map { perlinc => $_ }, @INC ],
+    mysqlVersion  => $mysqlVersion,
+    apacheVersion => $apacheVersion,
+    zebraVersion  => $zebraVersion,
+    components  => $sysprefs_arrayref,
+);
+
+
+print  $query->header,$template->output;
+
+sub sysprefs{
+    my $dbh= DBI->connect("DBI:mysql:dbname=koha;host=vmsql;port=3306",
+        "kohauser", "password");
+    return $dbh->selectall_arrayref( "SELECT * from systempreferences LIMIT 0,10", {Slice =>{} });
+}
