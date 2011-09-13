616925db6d513b15970d588914cb6c71ef85e81d
Matthias Meusburger <matthias.meusburger@biblibre.com>
2011-09-13 10:42:49 +0200

    MT7481: Allows to use tools/export.pl from commandline
     - use biblioitems.timestamp instead of items.timestamp

--- tools/export.pl
+++ tools/export.pl
@@ -177,7 +177,7 @@ if ( $op eq "export" ) {
             }
 
 	    if ($timestamp) {
-                $q .= " AND $itemstable.timestamp >= ? ";
+                $q .= " AND $biblioitemstable.timestamp >= ? ";
                 push @sql_params, $timestamp->output('iso');
             }
 
