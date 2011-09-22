a45d40a0f3c0872dd1dbddf1bec61c436f6be3fe
Stéphane Delaune <stephane.delaune@biblibre.com>
2011-09-22 12:02:33 +0200

    (MT #7044) add support of 4 new virtualhost variables for multiple opac
    OPAC_CSS_ABSOLUTE_OVERRIDE to override opac's css file with another defined by an absolute url
    OPAC_USER_CSS to use a specific css file defined with an absolute url who will be used at the same place than opacusercss's system preference
    OPAC_LIBRARYNAME_OVERRIDE to override the libraryname's system preference
    OPAC_LIMIT_NEWS_TO_SERVERNAME to force news to be displayed only with the same servername than the virtualhost
    (cherry picked from commit 479da56ede9d9eea30ff9b14e2ae815d234d58f3)
    
    Conflicts solved:
    
    	kohaversion.pl

--- installer/data/mysql/kohastructure.sql
+++ installer/data/mysql/kohastructure.sql
@@ -1369,6 +1369,7 @@ CREATE TABLE `opac_news` (
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP,
   `expirationdate` date default NULL,
   `number` int(11) default NULL,
+  `servername` text default NULL,
   PRIMARY KEY  (`idnew`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
