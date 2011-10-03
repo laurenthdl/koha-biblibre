46278fdd38dbd0aa9736c686e544f1c7d67fad02
Henri-Damien LAURENT <henridamien.laurent@biblibre.com>
2011-10-03 16:58:16 +0200

    MT8059 : Error 500 on member search
    When searching on a base where there is no accountlines for users, causes an error 500
    
    Can't use an undefined value as an ARRAY reference at /home/koha/src/C4/Members.pm
    line 731.
    This patch changes the way the DBI is invoked so that no error would be sent when no data.
    (cherry picked from commit dd18d321ebd92a35baeb75fe29de216418208bc2)

--- C4/Members.pm
+++ C4/Members.pm
@@ -716,7 +716,7 @@ sub GetMemberIssuesAndFines {
     $debug and warn $query . "\n";
     my $sth = $dbh->prepare($query);
     $sth->execute($borrowernumber);
-    my $issue_count = $sth->fetchrow_arrayref->[0];
+    my ($issue_count) = $sth->fetchrow_array;
 
     $sth = $dbh->prepare(
         "SELECT COUNT(*) FROM issues 
@@ -724,11 +724,11 @@ sub GetMemberIssuesAndFines {
          AND date_due < curdate()"
     );
     $sth->execute($borrowernumber);
-    my $overdue_count = $sth->fetchrow_arrayref->[0];
+    my ($overdue_count) = $sth->fetchrow_array;
 
     $sth = $dbh->prepare("SELECT SUM(amountoutstanding) FROM accountlines WHERE borrowernumber = ?");
     $sth->execute($borrowernumber);
-    my $total_fines = $sth->fetchrow_arrayref->[0];
+    my ($total_fines) = $sth->fetchrow_array;
 
     return ( $overdue_count, $issue_count, $total_fines );
 }
