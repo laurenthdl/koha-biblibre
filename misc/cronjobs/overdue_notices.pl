2ad8572080e494a0b1b8d8feed96603d9ddd5196
Julian Maurice <julian.maurice@biblibre.com>
2011-10-03 12:38:05 +0200

    MT7998: Fix formatting of html letters
    Do not replace '\n' with '<br/>' but enclose letter with '<pre></pre>'

--- misc/cronjobs/overdue_notices.pl
+++ misc/cronjobs/overdue_notices.pl
@@ -486,7 +486,6 @@ END_SQL
                 $verbose and warn "borrower $firstname, $lastname ($borrowernumber) has $itemcount items triggering level $i.";
 
                 my $letter = C4::Letters::getletter( 'circulation', $overdue_rules->{"letter$i"} );
-
                 unless ($letter) {
                     $verbose and warn "Message '$overdue_rules->{letter$i}' content not found";
 
@@ -623,9 +622,21 @@ END_SQL
         my $content = join(";", qw(title name surname address1 address2 zipcode city email itemcount itemsinfo due_date issue_date)) . "\n";
         $content .= join( "\n", map { m/(.+)/ } @output_chunks );
 
+        my $attachment_filename = ($csvfilename)
+            ? 'attachment.csv'
+            : ($htmlfilename)
+                ? 'attachment.html'
+                : 'attachment.txt';
+        my $attachment_type = ($csvfilename)
+            ? 'text/csv'
+            : ($htmlfilename)
+                ? 'text/html'
+                : 'text/plain';
+        $attachment_type .= qq/; charset="UTF-8"/;
+
         my $attachment = {
-            filename => defined $csvfilename ? 'attachment.csv' : 'attachment.txt',
-            type => 'text/plain',
+            filename => $attachment_filename,
+            type => $attachment_type,
             content => join( "\n", @output_chunks )
         };
 
@@ -764,9 +775,8 @@ sub prepare_letter_for_printing {
         }
     } elsif ( exists $params->{'outputformat'} && $params->{'outputformat'} eq 'html' ) {
         $return = "<pre>\n";
-        $params->{'letter'}->{'content'} =~ s##<br/>#g;
         $return .= "$params->{'letter'}->{'content'}\n";
-        $return .= "\n</pre>\n";
+        $return .= "</pre>\n";
     } else {
         $return .= "$params->{'letter'}->{'content'}\n";
 
